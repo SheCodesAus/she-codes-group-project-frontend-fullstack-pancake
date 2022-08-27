e011adfcb694ed144387678b576c20608e1f8b96
Miao Zhao <miao_zhao@yahoo.com>
2022-08-27 12:50:35 +1000

    Create HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Container from "../components/common/Container";
+
+export default function HomePage() {
+  return (
+    <>
+      <Container>Homepage content</Container>
+    </>
+  );
+}
