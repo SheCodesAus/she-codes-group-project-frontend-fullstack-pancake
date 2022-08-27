c0c11a2f80d021020bdc3f78551b1487d8957214
Miao Zhao <miao_zhao@yahoo.com>
2022-08-27 12:50:44 +1000

    Create SignUpPage.jsx

---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Container from "../components/common/Container";
+
+export default function SingUpPage() {
+  return (
+    <>
+      <Container>Sign up Page content</Container>
+    </>
+  );
+}
