26ec7d125f771b2075500dfba067ee2e8e6a4562
Miao Zhao <miao_zhao@yahoo.com>
2022-08-27 13:41:49 +1000

    Setup `Routes` in the App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/common/Nav";
+import HomePage from "./pages/HomePage";
+import AboutUsPage from "./pages/AboutUsPage";
+import AllWorkshopsPage from "./pages/AllWorshopsPage";
+import WorkshopDetailPage from "./pages/WorkshopDetailsPage";
+import CreateWorkshopPage from "./pages/CreateWorkshopPage";
+
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import Footer from "./components/common/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div id="App">
+        <Nav />
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/about-us" element={<AboutUsPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/all-workshops" element={<AllWorkshopsPage />} />
+          <Route path="/workshop/:id" element={<WorkshopDetailPage />} />
+          <Route path="/create-workshop" element={<CreateWorkshopPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
