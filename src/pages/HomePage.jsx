d55f78d9bcf9a2295699c49de0247bba682f160b
Emily Conaghan <emily@huntandco.io>
2022-08-30 18:51:06 +1000

    Implemented Two Column feature (Emily & Miao)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Container from "../components/common/Container";
+import TwoColumn from "../components/common/TwoColumn";
+import Placeholder from "../assets/images/placeholder.svg"
 
 export default function HomePage() {
   return (
     <>
-      <Container>Homepage content</Container>
+      <Container><TwoColumn heading='2' paragraph="hello" button='submit' image={Placeholder} link="/" direction="" /></Container>
     </>
   );
 }
