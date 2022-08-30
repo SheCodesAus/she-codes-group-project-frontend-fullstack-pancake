5a2017ef9169ce26fc1493bd2f0dfc44177195a3
Miao Zhao <miao_zhao@yahoo.com>
2022-08-31 08:56:52 +1000

    Render `AllWorkshopsPage` with `mockWorkshopsData`

---
+++ src/mock/workshopsData.js
@@ -0,0 +1,92 @@
+export const mockWorkshopsData = [
+  {
+    id: 1,
+    title: "Workshop One Title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, fames enim ullamcorper platea. Nibh dictum faucibus adipiscing dictum pellentesque a dictumst. Ornare amet, nec arcu enim nec libero diam amet amet. Purus ut quam nisl aenean elit, euismod proin augue. Amet, maecenas quis viverra quam. Ipsum vitae, vitae, in risus eu magna vel.",
+    location: "Sydney",
+    difficulty_level: "easy",
+    languages: ["Cloud Computing"],
+    image:
+      "https://images.unsplash.com/photo-1660551772352-0855c10356b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    date_due: "2022-09-20T14:28:23.382748Z",
+    is_open: true,
+    date_created: "2022-07-05T14:28:23.382748Z",
+    owner: 2,
+  },
+  {
+    id: 2,
+    title: "Workshop Two Title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, fames enim ullamcorper platea. Nibh dictum faucibus adipiscing dictum pellentesque a dictumst. Ornare amet, nec arcu enim nec libero diam amet amet. Purus ut quam nisl aenean elit, euismod proin augue. Amet, maecenas quis viverra quam. Ipsum vitae, vitae, in risus eu magna vel.",
+    location: "Brisbane",
+    difficulty_level: "middle",
+    languages: ["Django", "Python"],
+    image:
+      "https://images.unsplash.com/photo-1660893890933-d6708b7fa14a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=760&q=80",
+    date_due: "2022-11-17T00:00:00Z",
+    is_open: true,
+    date_created: "2022-08-20T07:45:01.460000Z",
+    owner: 7,
+  },
+  {
+    id: 3,
+    title: "Workshop Three Title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, fames enim ullamcorper platea. Nibh dictum faucibus adipiscing dictum pellentesque a dictumst. Ornare amet, nec arcu enim nec libero diam amet amet. Purus ut quam nisl aenean elit, euismod proin augue. Amet, maecenas quis viverra quam. Ipsum vitae, vitae, in risus eu magna vel.",
+    location: "Hobart",
+    difficulty_level: "advanced",
+    languages: ["Django", "Python"],
+    image:
+      "https://images.unsplash.com/photo-1660836521245-cdb2156050e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    date_due: "2022-08-04T00:00:00Z",
+    is_open: true,
+    date_created: "2022-08-20T07:59:58.670000Z",
+    owner: 7,
+  },
+  {
+    id: 4,
+    title: "Workshop Four Title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, fames enim ullamcorper platea. Nibh dictum faucibus adipiscing dictum pellentesque a dictumst. Ornare amet, nec arcu enim nec libero diam amet amet. Purus ut quam nisl aenean elit, euismod proin augue. Amet, maecenas quis viverra quam. Ipsum vitae, vitae, in risus eu magna vel.",
+    location: "Brisbane",
+    difficulty_level: "easy",
+    languages: ["React"],
+    image:
+      "https://images.unsplash.com/photo-1438012940875-4bf705025a8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    date_due: "2022-11-07T00:00:00Z",
+    is_open: true,
+    date_created: "2022-08-20T23:51:49.377000Z",
+    owner: 17,
+  },
+  {
+    id: 5,
+    title: "Workshop Five Title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, fames enim ullamcorper platea. Nibh dictum faucibus adipiscing dictum pellentesque a dictumst. Ornare amet, nec arcu enim nec libero diam amet amet. Purus ut quam nisl aenean elit, euismod proin augue. Amet, maecenas quis viverra quam. Ipsum vitae, vitae, in risus eu magna vel.",
+    location: "Brisbane",
+    difficulty_level: "advanced",
+    languages: ["HTML", "CSS"],
+    image:
+      "https://images.unsplash.com/photo-1518972734183-c5b490a7c637?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    date_due: "2022-09-20T14:28:23.382748Z",
+    is_open: true,
+    date_created: "2022-07-05T14:28:23.382748Z",
+    owner: 3,
+  },
+  {
+    id: 6,
+    title: "Workshop Six Title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus, fames enim ullamcorper platea. Nibh dictum faucibus adipiscing dictum pellentesque a dictumst. Ornare amet, nec arcu enim nec libero diam amet amet. Purus ut quam nisl aenean elit, euismod proin augue. Amet, maecenas quis viverra quam. Ipsum vitae, vitae, in risus eu magna vel.",
+    location: "Brisbane",
+    difficulty_level: "middle",
+    languages: ["JavaScript"],
+    image:
+      "https://images.unsplash.com/photo-1518972734183-c5b490a7c637?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    date_due: "2022-09-20T14:28:23.382748Z",
+    is_open: true,
+    date_created: "2022-07-05T14:28:23.382748Z",
+    owner: 3,
+  },
+];
--- src/pages/AllWorshopsPage.jsx
+++ src/pages/AllWorshopsPage.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import Container from "../components/common/Container";
 import ProjectCoverCard from "../components/ProjectCoverCard";
-import Placeholder from "../assets/images/placeholder.svg";
+import { mockWorkshopsData } from "../mock/workshopsData";
 
 export default function AllWorkshopsPage() {
-  const languages = [
-    { language: "HTML", tagColor: "tag-brand-primary" },
-    { language: "CSS", tagColor: "" },
-  ];
-
   return (
     <>
       <Container>
-        <ProjectCoverCard
-          image={Placeholder}
-          workshoptitle="Introduction to HTML"
-          languages={languages}
-          deliverymethod="Online"
-          difficulty="middle"
-          link="/"
-        />
+        {mockWorkshopsData.map((workshop, index) => {
+          return (
+            <ProjectCoverCard
+              key={index}
+              image={workshop.image}
+              workshoptitle={workshop.title}
+              languages={workshop.languages}
+              deliverymethod={workshop.location}
+              difficulty={workshop.difficulty_level}
+              link="/"
+            />
+          );
+        })}
       </Container>
     </>
   );
