5817ba45058f7625a67fe8b1bb0cffbf68808ba5
Miao Zhao <miao_zhao@yahoo.com>
2022-08-30 22:44:43 +1000

    Prettier only

--- src/components/common/TwoColumn/index.jsx
+++ src/components/common/TwoColumn/index.jsx
@@ -1,22 +1,30 @@
-import React from "react"
-import Container from "../Container"
-import CTAButton from "../CTAButton"
-import { Link, useNavigate } from "react-router-dom";
-import "./twocolumn.css"
+import React from "react";
+import CTAButton from "../CTAButton";
+import { Link } from "react-router-dom";
+import "./twocolumn.css";
 
-export default function TwoColumn({ heading, paragraph, button, image, link, direction }) {
-    return (
-        <div className={`columnContainer ${direction === "reverse" ? "reverse" : ""}`}>
-            <div className='columnText'>
-                <h2>{heading}</h2>
-                <p>{paragraph}</p>
-                <Link to={link}><CTAButton>{button}</CTAButton></Link>
-
-
-            </div>
-            <div className='columnImage'>
-                <img src={image}></img>
-            </div>
-        </ div>
-    );
+export default function TwoColumn({
+  heading,
+  paragraph,
+  button,
+  image,
+  link,
+  direction,
+}) {
+  return (
+    <div
+      className={`columnContainer ${direction === "reverse" ? "reverse" : ""}`}
+    >
+      <div className="columnText">
+        <h2>{heading}</h2>
+        <p>{paragraph}</p>
+        <Link to={link}>
+          <CTAButton variant="primary">{button}</CTAButton>
+        </Link>
+      </div>
+      <div className="columnImage">
+        <img src={image} alt={heading} />
+      </div>
+    </div>
+  );
 }
