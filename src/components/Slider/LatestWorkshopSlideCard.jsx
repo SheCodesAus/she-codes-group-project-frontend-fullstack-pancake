f2a53a73dcd7d53632c15f230613f83a7aa44f7d
Miao Zhao <miao_zhao@yahoo.com>
2022-09-02 17:26:53 +1000

    Update LatestWorkshopSlideCard.jsx

--- src/components/Slider/LatestWorkshopSlideCard.jsx
+++ src/components/Slider/LatestWorkshopSlideCard.jsx
@@ -1,25 +1,23 @@
 import SkillLevel from "../common/SkillLevel";
 import "./LatestWorkshopSlideCard.css";
 
-export default function LatestWorkshopSlideCard({ slides, current }) {
+export default function LatestWorkshopSlideCard({ slide }) {
   return (
     <div className="latest-workshop-slide-card">
       <div className="latest-workshop-slide-card-image">
-        <img src={slides[current].image} alt={slides[current].title} />
+        <img src={slide.image} alt={slide.title} />
       </div>
       <div className="latest-workshop-slide-card-text">
         <div className="latest-workshop-slide-card-text-row">
-          <h3>{slides[current].title}</h3>
-          <p>{slides[current].date_created}</p>
+          <h3>{slide.title}</h3>
+          <p>{slide.date_created}</p>
         </div>
         <div className="latest-workshop-slide-card-text-row">
-          <SkillLevel level={slides[current].difficulty_level} />
-          <span>{slides[current].location}</span>
+          <SkillLevel level={slide.difficulty_level} />
+          <span>{slide.location}</span>
         </div>
       </div>
-      <p className="latest-workshop-slide-card-p">
-        {slides[current].description}
-      </p>
+      <p className="latest-workshop-slide-card-p">{slide.description}</p>
     </div>
   );
 }
