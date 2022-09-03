d80f565396092b3f62c54bf850d67bec8caf31a9
Miao Zhao <miao_zhao@yahoo.com>
2022-09-03 23:42:09 +1000

    Refactor `Nav`

--- src/components/common/Nav/index.jsx
+++ src/components/common/Nav/index.jsx
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
-import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Container from "../Container";
 import CTAButton from "../CTAButton";
 import Logo from "../../../assets/logo/logo-vertical-color.svg";
+import StaticMenuItems from "./StaticMenuItems";
+import { getUserById } from "../../../services/users/getUserById";
 import "./nav.css";
+import AuthenticatedMenuItems from "./AuthenticatedMenuItems";
 
 export default function Nav() {
-  const navigate = useNavigate();
   const path = useLocation().pathname;
+  const [navUsername, setNavUsername] = useState("");
   const loginStatus = window.localStorage.getItem("login");
   const userId = window.localStorage.getItem("user_id");
+
+  if (userId) {
+    getUserById(userId).then(data => {
+      const { username } = data;
+      setNavUsername(username);
+    });
+  }
 
   return (
     <nav>
@@ -18,25 +28,12 @@ export default function Nav() {
         wrapperVariant="nav"
       >
         <div className="menu_items_wrapper">
-          {/* static menu items */}
-          <div className="static_menu_items">
-            <Link to="/" className="menu_item_home">
-              Home
-            </Link>
-            <Link to="/about-us" className="menu_item_home">
-              About Us
-            </Link>
-            <Link to="/all-workshops" className="menu_item_home">
-              Workshops
-            </Link>
-          </div>
+          <StaticMenuItems />
 
-          {/* logo */}
           <Link to="/" className="header-logo-link">
             <img src={Logo} alt="Thinkle_logo" className="header-logo" />
           </Link>
 
-          {/* dynamic menu items */}
           {!loginStatus ? (
             <div className="menu_item_auth_group">
               <Link to="/get-started">
@@ -44,32 +41,7 @@ export default function Nav() {
               </Link>
             </div>
           ) : (
-            <div className="menu_item_auth_group">
-              <Link to={`/profile/${userId}`}>
-                {/* <div className="nav-authenticated">
-                  <img
-                    src={navAvatar}
-                    alt={`${navUsername}`}
-                    onError={useDefaultAvatar}
-                    className="nav-avatar-image"
-                  />
-                  {navUsername}
-                </div> */}
-              </Link>
-              <Link to="/create-project">
-                <CTAButton>Create Project</CTAButton>
-              </Link>
-              <CTAButton
-                color="secondary"
-                onClick={() => {
-                  localStorage.clear();
-                  navigate("/");
-                  window.location.reload();
-                }}
-              >
-                Log out
-              </CTAButton>
-            </div>
+            <AuthenticatedMenuItems navUsername={navUsername} userId={userId} />
           )}
         </div>
       </Container>
