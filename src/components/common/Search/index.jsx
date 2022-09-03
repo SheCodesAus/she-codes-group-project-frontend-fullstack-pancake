6ea66145ba62e62540aef1fbd25bdb075638e406
Mrsschalkwyk <96808682+Mrsschalkwyk@users.noreply.github.com>
2022-09-03 12:50:35 +1000

    search filter finish

--- src/components/common/Search/index.jsx
+++ src/components/common/Search/index.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 // import { Link, useNavigate } from "react-router-dom";
-import "../App.js"
+// import "../App.js"
 import "./search.css";
-import MOCK_DATA from ".\src\MOCK_DATA.json";
+import MOCK_DATA from "./MOCK_DATA.json";
 import {useState} from "react";
 
 
 
     function Search() {
         // const navigate = useNavigate();
+        console.log('MOCK DATA', MOCK_DATA)
 
         const [searchTerm,setSearchTerm] = useState('')
         return (
-        <div className="Search">
+        <div className="search">
             <input type="text" placeholder="Search..." 
             onChange={event => {
                 setSearchTerm(event.target.value);
@@ -21,13 +22,20 @@ import {useState} from "react";
             {MOCK_DATA.filter((val)=> {
                 if (searchTerm =="") {
                     return val
-                } else if (val.language.toLowerCase().includes(searchTerm.toLowerCase())){
+                }
+                
+                if (val.language.toLowerCase().includes(searchTerm.toLowerCase()) || val.city.toLowerCase().includes(searchTerm.toLowerCase()) || val.datetime.toLowerCase().includes(searchTerm.toLowerCase())){ 
+                // if (val.city.toLowerCase().includes(searchTerm.toLowerCase()))
+                // if (val.datetime.toLowerCase().includes(searchTerm.toLowerCase()))
                     return val 
                 }
             }).map((val, key) => {
                 return (
                 <div className="user" key={key}> 
-                    <p>{val.language}</p>
+                    <ol>
+                    {val.language}&nbsp; 
+                    {val.city }
+                    </ol>
                 </div>
                 );
             })}
