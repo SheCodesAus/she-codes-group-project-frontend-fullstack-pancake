6d4fe7ef2869d8e1d3f40621a10b878e807e85cf
Miao Zhao <miao_zhao@yahoo.com>
2022-09-04 20:01:16 +1000

    Update `GetStarted` with `SignUpForm`

--- src/components/modules/GetStarted/index.jsx
+++ src/components/modules/GetStarted/index.jsx
@@ -1,19 +1,39 @@
 import { useState } from "react";
 import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
 import "./index.css";
 
 export default function GetStarted() {
   const [isMember, setIsMember] = useState(true);
+  const [submitMessage, setSubmitMessage] = useState("");
+  const [submitSuccess, setSubmitSuccess] = useState(false);
 
   return (
     <div className="auth-form-container">
-      <h2 className="auth-title">{isMember ? "Login" : "Register"}</h2>
-      {isMember ? <LoginForm /> : ""}
+      <h2 className="auth-title">{isMember ? "Login" : "Sign up"}</h2>
+      {isMember ? (
+        <LoginForm
+          setSubmitMessage={setSubmitMessage}
+          setSubmitSuccess={setSubmitSuccess}
+        />
+      ) : (
+        <SignUpForm
+          setIsMember={setIsMember}
+          setSubmitMessage={setSubmitMessage}
+          setSubmitSuccess={setSubmitSuccess}
+        />
+      )}
+      <p className={`submit-message ${submitSuccess ? "success" : "fail"}`}>
+        {submitMessage}
+      </p>
       <p className="auth-question">
         {isMember ? "Not a member yet ?" : "Already a member ?"}
         <span
           type="button"
-          onClick={() => setIsMember(!isMember)}
+          onClick={() => {
+            setIsMember(!isMember);
+            setSubmitMessage("");
+          }}
           className="member-btn"
         >
           {isMember ? " Sign up" : " Log in"}
