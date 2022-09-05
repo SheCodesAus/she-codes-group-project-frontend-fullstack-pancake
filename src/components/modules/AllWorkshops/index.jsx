2f8427562f93a189eae57c0d56697117e65bd60c
Miao Zhao <miao_zhao@yahoo.com>
2022-09-05 23:03:41 +1000

    Add `Sort` into `AllWorkshops`

--- src/components/modules/AllWorkshops/index.jsx
+++ src/components/modules/AllWorkshops/index.jsx
@@ -3,13 +3,21 @@ import Grid from "../../common/Grid";
 import FilteredList from "./FilteredList";
 import SearchBar from "./SearchBar";
 import "./index.css";
+import Sort from "./Sort";
 
 export default function AllWorkshops({ data }) {
   const [filteredData, setFilteredData] = useState(data);
 
   return (
     <>
-      <SearchBar data={data} setFilteredData={setFilteredData} />
+      <div className="search-filter-sort-container">
+        <Sort
+          data={data}
+          filteredData={filteredData}
+          setFilteredData={setFilteredData}
+        />
+        <SearchBar data={data} setFilteredData={setFilteredData} />
+      </div>
       <Grid>
         <FilteredList filteredData={filteredData} />
       </Grid>
