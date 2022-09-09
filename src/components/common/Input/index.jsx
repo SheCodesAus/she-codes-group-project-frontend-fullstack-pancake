310deab3f2fb632a0b554abaea5b4bc61e296e8f
Miao Zhao <miao_zhao@yahoo.com>
2022-09-09 20:35:20 +1000

    Add `checkbox` type for `Input` component

--- src/components/common/Input/index.jsx
+++ src/components/common/Input/index.jsx
@@ -5,15 +5,35 @@ export default function Input(props) {
   const inputType = type => {
     switch (type) {
       case "textarea":
-        return <textarea className="form-textarea" {...props} />;
+        return (
+          <>
+            <label htmlFor={props.id}>{props.label}</label>
+            <textarea className="form-textarea" {...props} />
+          </>
+        );
       case "select":
         return (
-          <select className="form-input" {...props}>
-            {props.children}
-          </select>
+          <>
+            <label htmlFor={props.id}>{props.label}</label>
+            <select className="form-input" {...props}>
+              {props.children}
+            </select>
+          </>
+        );
+      case "checkbox":
+        return (
+          <div className="form-checkbox">
+            <input {...props}></input>
+            <label htmlFor={props.id}>{props.label}</label>
+          </div>
         );
       default:
-        return <input className="form-input" {...props} />;
+        return (
+          <>
+            <label htmlFor={props.id}>{props.label}</label>
+            <input className="form-input" {...props} />
+          </>
+        );
     }
   };
 
@@ -38,11 +58,10 @@ export default function Input(props) {
   return (
     <>
       <div
-        className={`form-field ${hasLabel(props.label)} ${toggleWidth(
+        className={`form-field ${hasLabel(props.label)}  ${toggleWidth(
           props.width
         )}`}
       >
-        <label htmlFor={props.id}>{props.label}</label>
         {inputType(props.type)}
       </div>
     </>
