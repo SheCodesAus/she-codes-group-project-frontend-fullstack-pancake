8a7a1a9a7bf7f5ae8758a7c6092ae2f9f6a98a61
Miao Zhao <miao_zhao@yahoo.com>
2022-09-10 20:39:57 +1000

    Add style to `UserProfilePage` for the username and avatar

---
+++ src/pages/UserProfilePage/index.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Loading from "../../components/common/Loading";
+import Container from "../../components/common/Container";
+import Banner from "../../components/common/Banner";
+import Profile from "../../components/modules/Profile";
+import { getUserById } from "../../services/users/getUserById";
+import { useDefaultAvatar } from "../../utilities/error/useDefaultAvatar";
+import "./style.css";
+
+export default function UserProfilePage() {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState("");
+  const [userAvatar, setUserAvatar] = useState("");
+  useEffect(() => {
+    getUserById(id).then(data => {
+      console.log("user data: ", data);
+      setUsername(data.username);
+      setUserAvatar(data.profile_image);
+      setLoading(false);
+    });
+  }, [id]);
+
+  return (
+    <>
+      <Container wrapperVariant="banner">
+        <Banner>
+          <div className="user-banner">
+            {loading ? (
+              <Loading />
+            ) : (
+              <img
+                src={userAvatar}
+                alt={username}
+                onError={useDefaultAvatar}
+                className="user-avatar"
+              />
+            )}
+            <h1>{loading ? "Fetching username ... " : username}</h1>
+          </div>
+        </Banner>
+      </Container>
+      <Container>
+        <Profile userId={id} />
+      </Container>
+    </>
+  );
+}
