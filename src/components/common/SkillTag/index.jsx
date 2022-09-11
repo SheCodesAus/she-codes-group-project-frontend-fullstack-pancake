b9da874bca06fff8e06b6186c3723190229f7146
Miao Zhao <miao_zhao@yahoo.com>
2022-09-11 15:18:39 +1000

    Update `SkillTag` style

--- src/components/common/SkillTag/index.jsx
+++ src/components/common/SkillTag/index.jsx
@@ -2,22 +2,15 @@ import React from "react";
 import "./style.css";
 
 const selectTagColor = children => {
-  if (
-    children === "html" ||
-    children === "javascript" ||
-    children === "python"
-  ) {
+  if (children === "html" || children === "aws" || children === "python") {
     return "tag-brand-primary";
   }
-  if (
-    children === "cloud computing" ||
-    children === "react" ||
-    children === "django"
-  ) {
+  if (children === "react" || children === "django") {
     return "tag-brand-secondary";
   }
   return "";
 };
+
 export default function SkillTag({ children }) {
   return (
     <span className={`SkillTag ${selectTagColor(children.toLowerCase())}`}>
