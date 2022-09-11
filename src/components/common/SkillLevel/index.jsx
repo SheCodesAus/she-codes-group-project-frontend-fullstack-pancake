a4fb83e9c821b8e0c846a6ef4df7cb6b00ecad25
Miao Zhao <miao_zhao@yahoo.com>
2022-09-11 15:56:22 +1000

    Update `SkillLevel` style with text element

--- src/components/common/SkillLevel/index.jsx
+++ src/components/common/SkillLevel/index.jsx
@@ -1,24 +1,40 @@
 import React from "react";
 import "./style.css";
 
-export default function SkillLevel({ level }) {
+export default function SkillLevel({ level, showText }) {
+  const switchLevel = level => {
+    if (level === "EL" || level === "Entry-level") {
+      return "Entry Level";
+    }
+    if (level === "Intermediate") {
+      return "Intermediate";
+    }
+    if (level === "Advanced") {
+      return "Advanced";
+    }
+  };
   return (
-    <div className="skill-level-group">
-      <div
-        className={`skill-level level-easy ${
-          level === "EL" || level === "Entry-level" ? "level-selected" : ""
-        }`}
-      ></div>
-      <div
-        className={`skill-level level-middle ${
-          level === "Intermediate" ? "level-selected" : ""
-        }`}
-      ></div>
-      <div
-        className={`skill-level level-advanced ${
-          level === "Advanced" ? "level-selected" : ""
-        }`}
-      ></div>
+    <div class="skill-level-container">
+      <div className="skill-level-group">
+        <div
+          className={`skill-level level-easy ${
+            level === "EL" || level === "Entry-level" ? "level-selected" : ""
+          }`}
+        ></div>
+        <div
+          className={`skill-level level-middle ${
+            level === "Intermediate" ? "level-selected" : ""
+          }`}
+        ></div>
+        <div
+          className={`skill-level level-advanced ${
+            level === "Advanced" ? "level-selected" : ""
+          }`}
+        ></div>
+      </div>
+      {showText && (
+        <span className="skill-level-text">{switchLevel(level)}</span>
+      )}
     </div>
   );
 }
