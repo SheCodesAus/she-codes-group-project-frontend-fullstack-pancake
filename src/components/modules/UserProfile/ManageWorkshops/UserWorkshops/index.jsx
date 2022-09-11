303aecab352ebdd00a7712bcb1df4be621b979ec
Miao Zhao <miao_zhao@yahoo.com>
2022-09-11 21:52:20 +1000

    Add delete workshop feature

--- src/components/modules/UserProfile/ManageWorkshops/UserWorkshops/index.jsx
+++ src/components/modules/UserProfile/ManageWorkshops/UserWorkshops/index.jsx
@@ -1,26 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Grid from "../../../../common/Grid";
+import { deleteWorkshopById } from "../../../../../services/workshops/deleteWorkshopById";
 import { useErrorImage } from "../../../../../utilities/error/useErrorImage";
 import "./style.css";
-import { Link } from "react-router-dom";
 
 export default function UserWorkshops({ userWorkshops }) {
+  const [modal, setModal] = useState(false);
+  const token = window.localStorage.getItem("token");
+
+  const handleDeleteClick = async id => {
+    await deleteWorkshopById(id, token);
+    window.location.reload();
+  };
+
   return (
     <Grid>
       {userWorkshops.map((userWorkshop, index) => {
         const { id, title, image } = userWorkshop;
         return (
-          <div key={index} className="user-workshop-card">
-            <div className="user-workshop-card-image">
-              <img src={image} alt={title} onError={useErrorImage} />
+          <div key={index}>
+            <div className="user-workshop-card">
+              <div className="user-workshop-card-image">
+                <img src={image} alt={title} onError={useErrorImage} />
+              </div>
+              <div className="user-workshop-card-manage">
+                <h4>{title}</h4>
+                <div className="button-group buttons-active">
+                  <button
+                    className="button-secondary"
+                    onClick={() => setModal(true)}
+                  >
+                    Delete
+                  </button>
+                  <Link to={`/update-workshop/${id}`}>
+                    <button className="button-primary">Update</button>
+                  </Link>
+                </div>
+              </div>
             </div>
-            <div className="user-workshop-card-manage">
-              <h4>{title}</h4>
-              <div className="button-group buttons-active">
-                <button className="button-danger">Delete</button>
-                <Link to={`/update-workshop/${id}`}>
-                  <button className="button-primary">Update</button>
-                </Link>
+            <div
+              className={`delete-confirm-modal ${
+                modal ? "modal-display" : "modal-hidden"
+              }`}
+            >
+              <p>
+                Do you confirm to delete this project? Once it is deleted, you
+                cannot recover it
+              </p>
+              <div className="button-group">
+                <button
+                  onClick={() => setModal(false)}
+                  className="button-secondary full"
+                >
+                  No, not now
+                </button>
+                <button
+                  onClick={() => handleDeleteClick(id)}
+                  className="button-danger full"
+                >
+                  Yes, delete now
+                </button>
               </div>
             </div>
           </div>
