c171d9dab4cbb863bd4172aaba32441c89b9597a
Miao Zhao <miao_zhao@yahoo.com>
2022-09-13 07:42:29 +1000

    Add title to create workshop page

--- src/pages/CreateWorkshopPage.jsx
+++ src/pages/CreateWorkshopPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Container from "../components/common/Container";
+import Banner from "../components/common/Banner";
 import CreateWorkshopForm from "../components/modules/CreateWorkshop";
 
 export default function CreateWorkshopPage() {
   return (
     <>
+      <Container wrapperVariant="banner">
+        <Banner variant="center-align">Create a workshop</Banner>
+      </Container>
       <Container>
         <CreateWorkshopForm />
       </Container>
